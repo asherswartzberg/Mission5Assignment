c227fa334f191e0a3466110f0b37371dd5ed249d
asherswartzberg <agswartzberg@gmail.com>
2026-02-06 17:20:48 -0700

    Add project files.

---
+++ Mission5Assignment/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$("#calculate").click(function () {
+    const hours = Number($("#hours").val());
+    const cost = Number($("#cost").val());
+
+    if (isNaN(hours) || hours < 0) {
+        alert("Enter a valid number");
+        return;
+    }
+
+    const result = hours * cost;
+    $("#result").val(result.toFixed(2));
+});
